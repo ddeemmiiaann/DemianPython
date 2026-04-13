4715932d3aaecf514f919aecce4c2503336c8504
ddeemmiiaann <demianavalos.c@gmail.com>
2026-04-13 09:47:29 -0400

    second comit

--- Prueba.py
+++ Prueba.py
@@ -1,1 +1,18 @@
-print("Dejenme renuncias al MacDonaaaal")
+print("Dejenme renunciar al MacDonaaaal")
+
+# Creando variables
+
+titulo="clima de hoy" #string
+diaDelmes=13 #int
+mes=4 #int
+temperatura = 22.3 #float
+llueve=False #boolean
+
+print(titulo)
+print("Dia del mes ", diaDelmes)
+print("Estamos en el mes ", mes)
+print("La temperatura es ", temperatura, " grados")
+if llueve==True:
+    print("Hoy debe llevar paraguas")
+else: 
+    print("Hoy se puede llevar polera mangacorta")
